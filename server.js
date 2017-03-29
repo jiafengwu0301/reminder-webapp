b11e0189f30734aaf4a6f1fdfbbb76fc701b97a7
jiafengwu0301 <jiafengwu0301@gmail.com>
2017-03-28 21:33:19 -0600

    Server port change

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set('port', (process.env.PORT || 8000));
+
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', function(req, res){
     res.sendFile('/index.html');
 });
 
-app.listen(8000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
