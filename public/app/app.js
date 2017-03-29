630ed50374099bb64e6139a198a219145cb97e5f
jiafengwu0301 <jiafengwu0301@gmail.com>
2017-03-28 21:22:22 -0600

    Put all static file in folder called public, add nodejs

---
+++ public/app/app.js
@@ -0,0 +1,24 @@
+angular
+    .module('app', ['ngRoute','ui.bootstrap.datetimepicker','gm'])
+    .config(['$routeProvider','$locationProvider',
+        function($routeProvider,$locationProvider){
+
+            $routeProvider.when('/',{
+                templateUrl: 'views/main.html',
+                controller: 'homeController',
+                controllerAs: 'vm'
+            })
+
+            .when('/new',{
+                templateUrl: 'views/newItem.html',
+                controller: 'newItemController',
+                controllerAs: 'vm'
+            })
+
+            .when('/item/:id',{
+                templateUrl: 'views/item.html',
+                controller: 'itemController',
+                controllerAs: 'vm'
+            });
+        }
+    ]);
